e8322079c677b4546efd53612f58c38946fb385b
Karter <kphillips40@student.cscc.edu>
2020-05-27 22:12:37 -0400

    Improved comments

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,17 +4,25 @@ import gui.NavEvent;
 import model.Bug;
 import model.Database;
 import model.PriorityCategory;
-
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+/**
+ * Communication medium from the gui to the model
+ */
 public class Controller {
     Database db = new Database();
 
     public List<Bug> getBugs() {
-        List<Bug> list = db.getBugs();
-        return list;
+        return db.getBugs();
     }
 
+    /**
+     * Takes bug form submission event, separates the field data, stores into new Bug object,
+     * then sends that Bug object to the addBug method in Database.java
+     * @param e - NavEvent object
+     */
     public void addBug(NavEvent e) {
         String project = e.getProject();
         int priorityCatId = e.getPriorityCat();
@@ -35,4 +43,20 @@ public class Controller {
         Bug bug = new Bug(project, priorityCategory, description);
         db.addBug(bug);
     }
+    /**
+     * Stores data into a file
+     * @param file - File object
+     * @throws IOException - outputting to file
+     */
+   public void saveToFile(File file) throws IOException {
+        db.saveToFile(file);
+   }
+    /**
+     * Retrieves data from a file
+     * @param file - File object
+     * @throws IOException - outputting to file
+     */
+   public void loadFromFile(File file) throws IOException {
+        db.loadFromFile(file);
+   }
 }
