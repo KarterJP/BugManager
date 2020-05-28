d2742e0951c27bd6aaadf0be4536a30a9844a013
Karter <kphillips40@student.cscc.edu>
2020-05-27 20:40:36 -0400

    Controller for gui-to-database communications

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,38 @@
 package controller;
 
+import gui.NavEvent;
+import model.Bug;
+import model.Database;
+import model.PriorityCategory;
+
+import java.util.List;
+
 public class Controller {
+    Database db = new Database();
+
+    public List<Bug> getBugs() {
+        List<Bug> list = db.getBugs();
+        return list;
+    }
+
+    public void addBug(NavEvent e) {
+        String project = e.getProject();
+        int priorityCatId = e.getPriorityCat();
+        String description = e.getDescription();
+
+        PriorityCategory priorityCategory;
+
+        if (priorityCatId == 0) {
+            priorityCategory = PriorityCategory.low;
+        }
+        else if (priorityCatId == 1) {
+            priorityCategory = PriorityCategory.medium;
+        }
+        else {
+            priorityCategory = PriorityCategory.high;
+        }
+
+        Bug bug = new Bug(project, priorityCategory, description);
+        db.addBug(bug);
+    }
 }
